49128307a7a5c8a0b2dec63bb507a55d4ecd77ae
UnlistedTester0 <eisenbraunmm09@gmail.com>
2026-08-30 03:19:09 -0600

    Renamed pages folder to src_pages to forcefully wipe out broken app link

--- src_pages/4_Rock_Climbing.py
+++ src_pages/4_Rock_Climbing.py
@@ -98,7 +98,7 @@ with nav_row2_col3:
 st.markdown("---")
 
 # ROW 3: Return Destination Vector
-PAGE_HOME_LOBBY = f"{folder}/0_Main_Menu.py"
+PAGE_HOME_LOBBY = src_pages/0_Main_Menu.py"
 
 if st.button("⬅️ Back to Main Menu", use_container_width=True):
     st.switch_page(PAGE_HOME_LOBBY)
